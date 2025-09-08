0a12ac5f4905ac1fb371ea6817d7322f08a744f9
Nathaniel Greisen <34870485+doggomeloon@users.noreply.github.com>
2025-09-08 13:16:50 -0700

    Attempted Infinite Parallax
    Not currently working

---
+++ 2D Test/Assets/InfiniteParallax.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class InfiniteParallax : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float parallaxFactor; // 0 = static, 1 = camera speed
+    private Vector3 lastCameraPosition;
+
+    private float spriteWidth;
+    private Transform[] children;
+
+    void Start()
+    {
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
+
+        lastCameraPosition = cameraTransform.position;
+
+        // Get width of sprite
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        spriteWidth = sr.bounds.size.x;
+    }
+
+    void Update()
+    {
+        Vector3 delta = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(delta.x * parallaxFactor, 0, 0);
+        lastCameraPosition = cameraTransform.position;
+
+        // Reposition for infinite scrolling
+        if (cameraTransform.position.x > transform.position.x + spriteWidth*2)
+        {
+            transform.position += new Vector3(spriteWidth, 0, 0);
+        }
+        else if (cameraTransform.position.x < transform.position.x - spriteWidth*2)
+        {
+            transform.position -= new Vector3(spriteWidth, 0, 0);
+        }
+    }
+}
